edbb2f68e89700828947535c7aa040d46e889bd7
Masayuki Hiyama <hiiyan0402@gmail.com>
2025-10-22 11:47:20 +0900

    change deployment processes in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,19 @@ APP_PLAN_NAME="aoai-demo-apps-plan"
 APP_NAME="auto-presentation-"$(date +%s)
 SKU='B3'
 
-# アプリケーションをデプロイする
-az webapp up \
+# App Service プランを作成
+az appservice plan create \
+    --name $APP_PLAN_NAME \
     --resource-group $RESOURCE_GROUP \
+    --sku $SKU \
+    --is-linux \
+    --location $REGION
+
+# Azure Web App を作成
+az webapp create \
     --name $APP_NAME \
     --plan $APP_PLAN_NAME \
-    --sku $SKU \
-    --location $REGION \
+    --resource-group $RESOURCE_GROUP \
     --runtime 'PYTHON:3.11'
 
 # Azure Web Apps の環境変数
@@ -32,3 +38,12 @@ az webapp config set \
     --resource-group $RESOURCE_GROUP \
     --name $APP_NAME \
     --startup-file "python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0"
+
+# アプリケーションをデプロイする
+az webapp up \
+    --resource-group $RESOURCE_GROUP \
+    --name $APP_NAME \
+    --plan $APP_PLAN_NAME \
+    --sku $SKU \
+    --location $REGION \
+    --runtime 'PYTHON:3.11'
